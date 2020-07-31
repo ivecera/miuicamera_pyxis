5595d0a49a3488ef6b58071c7fd140fc1ede4745
Ivan Vecera <ivan@cera.cz>
2020-07-31 09:18:23 +0200

    Require Android 10
    Signed-off-by: Ivan Vecera <ivan@cera.cz>

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@
 # Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
 # Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
 # Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-#MINAPI=20
+MINAPI=29
 #MAXAPI=25
 #DYNLIB=true
 #DEBUG=true
